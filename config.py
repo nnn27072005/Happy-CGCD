876cc7fb43ce9924f241533ce61ca0dce1fd6595
Nguyen Ngoc Ngu <ngu.nguyen2775@hcmut.edu.vn>
2025-11-25 19:51:25 +0700

    Update dataset paths for Kaggle environment

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 # -----------------
 # DATASET ROOTS
 # -----------------
-cifar_10_root = '/data4/datasets/CIFAR/'
-cifar_100_root = '/data4/datasets/CIFAR/'
-tiny_imagenet_root = '/data4/datasets/TinyImageNet/'
-cub_root = '/data4/datasets/gcd/CUB/'
-aircraft_root = '/data4/datasets/gcd/FGVC-Aircraft/fgvc-aircraft-2013b/'
-car_root = "/data4/datasets/gcd/Stanford-Cars/"
-herbarium_dataroot = '/data4/datasets/gcd/Herbarium19-Small/'
-imagenet_root = '/data4/sjma/dataset/ImageNet/ILSVRC2012/imagenet/'
+cifar_10_root = '/kaggle/input/cifar-10-python/cifar-10-batches-py'
+cifar_100_root = '/kaggle/input/cifar100/'
+tiny_imagenet_root = '/kaggle/input/tiny-imagenet-200/tiny-imagenet-200'
+cub_root = '/kaggle/input/cub-200-2011/CUB_200_2011'
+aircraft_root = '/kaggle/input/fgvc-aircraft-2013b/fgvc-aircraft-2013b'
+car_root = "/kaggle/input/stanford-cars-dataset/stanford-cars-dataset"
+herbarium_dataroot = '/kaggle/input/herbarium19-small/herbarium19-small'
+imagenet_root = '/kaggle/input/imagenet-1k/imagenet/'
 
 # OSR Split dir
-osr_split_dir = '/data4/datasets/gcd/ssb_splits/'
+osr_split_dir = 'ssb_splits/'
 
 # -----------------
 # OTHER PATHS
 # -----------------
-dino_pretrain_path = '/data6/home/sjma/.cache/torch/hub/checkpoints/dino_vitbase16_pretrain.pth'
-feature_extract_dir = '/lustre/home/sjma/Continual-GCD-project/GCD-baseline/extracted_features_public_impl'     # Extract features to this directory
-exp_root_happy = '/lustre/home/sjma/Continual-GCD-project/MyCGCD-v3/dev_outputs_Happy'          # All logs and checkpoints will be saved here
+dino_pretrain_path = 'dino_pretrain_path/dino_vitbase16_pretrain.pth'
+feature_extract_dir = '/kaggle/working/extracted_features'     # Extract features to this directory
+exp_root_happy = '/kaggle/working/dev_outputs_Happy'          # All logs and checkpoints will be saved here
